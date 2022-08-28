eebc7608a7fe0d374e6d4bcb61c2ff17e63c6a6d
YuuuJeong <dbwjdgh03@ajou.ac.kr>
2022-08-29 00:57:45 +0900

    feat: aws db 연결 및 회원가입 로그인 일부 개발

---
+++ src/backend/passport/index.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+const local = require('./localStrategy');
+const User = require('../models/user');
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user.email)
+    });
+
+    passport.deserializeUser(async(email, done) => {
+        User.findOne({where: { email }})
+        .then(user => done(null, user))
+        .catch(err => done(err))
+    });
+    local();
+};
