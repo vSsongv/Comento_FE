e9e089c227c5387dcce2549648e8d0c7ae6b8577
vSsongv <songdh21@naver.com>
2022-08-30 03:39:30 +0900

    answer test 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { RecoilRoot } from "recoil";
-import GlobalStyle from "./frontend/styles/GlobalStyle";
-import { theme } from "./frontend/styles/theme";
-import Home from "./frontend/components/pages/Home";
-import Login from "./frontend/components/pages/Login";
-import HeaderContainer from "./frontend/components/UI/organisms/HeaderContainer";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from './frontend/styles/GlobalStyle';
+import { theme } from './frontend/styles/theme';
+import Home from './frontend/components/pages/Home';
+import Login from './frontend/components/pages/Login';
+import HeaderContainer from './frontend/components/UI/organisms/HeaderContainer';
+import Answer from './frontend/components/pages/Answer';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <RecoilRoot>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
           <BrowserRouter>
             <HeaderContainer />
             <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/login" element={<Login />}></Route>
+              <Route path='/' element={<Answer />}></Route>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/answer' element={<Answer />}></Route>
             </Routes>
           </BrowserRouter>
         </ThemeProvider>
