7f81db78d8bc9f740b75aa6e2cde9170b4b6eca3
vSsongv <songdh21@naver.com>
2022-08-30 06:29:14 +0900

    ✨ feat: answer page layout ver1

--- src/frontend/components/pages/Answer.tsx
+++ src/frontend/components/pages/Answer.tsx
@@ -1,5 +1,60 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export default function Answer() {
-  return <div>Answer</div>;
+const BoxContainer = styled.div`
+  width: 1180px;
+  height: 593px;
+  margin: auto;
+  display: flex;
+`;
+
+const QuestionListContainer = styled.div`
+  width: 405px;
+  height: 595px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  background-color: green;
+  margin-top: 50px;
+`;
+
+const ListInfo = styled.div`
+  width: 405px;
+  height: 55px;
+  background-color: aqua;
+`;
+
+const QuestionContainer = styled.div`
+  width: 735px;
+  height: 595px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+  background-color: yellow;
+  margin-top: 50px;
+`;
+
+const QuestionInfo = styled.div`
+  width: 735px;
+  height: 75px;
+  background-color: olive;
+`;
+
+function Answer() {
+  return (
+    <>
+      <BoxContainer>
+        <QuestionListContainer>
+          <ListInfo></ListInfo>
+        </QuestionListContainer>
+        <QuestionContainer>
+          <QuestionInfo></QuestionInfo>
+        </QuestionContainer>
+      </BoxContainer>
+    </>
+  );
 }
+
+export default Answer;
