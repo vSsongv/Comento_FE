c0b29d330430e850e5062eaabc4bf402577613c6
tmin002 <ssh9930@gmail.com>
2022-09-28 21:55:06 +0900

    fix: Home.tsx에 첫번째 페이지 추가 나머지는 동연님과 같이 합칠 부분들

--- src/frontend/components/pages/Home.tsx
+++ src/frontend/components/pages/Home.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Button from "../UI/atoms/Button";
+import MainFirst from "../pages/main/MainFirst";
 
 function Home() {
   return (
-    <div>
-      홈페이지 입니다
-      <Button color="white" gradient={true} border={false} long={true}>
-        로그인
-      </Button>
-    </div>
+    <>
+      <MainFirst />
+    </>
   );
 }
 
