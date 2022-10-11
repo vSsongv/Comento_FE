cc00d41caea0db798d079dcfb9632068c2504c68
vSsongv <songdh21@naver.com>
2022-10-11 22:41:51 +0900

    ♻️refactoring: Footer code refactoring

--- src/frontend/components/UI/molescules/Footer.tsx
+++ src/frontend/components/UI/molescules/Footer.tsx
@@ -1,44 +1,30 @@
-import React from "react";
-import { theme } from "./../../../styles/theme";
-import styled from "styled-components";
-
-const Positioner = styled.div`
-    height: auto;
-    min-height: 100%;
-    position: relative;
-`;
-
-const BlackBackground = styled.div`
-    background-color: black;
-    display: flex;
-    justify-content: center;
-    height: auto;
-`;
+import React from 'react';
+import { theme } from './../../../styles/theme';
+import styled from 'styled-components';
 
 const FooterContents = styled.div`
-    width: 100%;
-    height: 164px;
-    display: flex;
-    flex-direction: row;
-    padding-top: 2rem;
-    padding-left: 4rem;
-    padding-right: 4rem;
+  width: 100%;
+  height: 100%;
+  margin-top: 100px;
+  padding-bottom: 80px;
+  display: flex;
+  background-color: black;
+  flex-direction: row;
 `;
 
 const Logo = styled.div`
-    font-size: 1.4rem;
-    font-weight: blod;
-    color: ${theme.color.primaryColor};
+  padding-top: 2rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
+  font-size: 1.4rem;
+  font-weight: blod;
+  color: ${theme.color.primaryColor};
 `;
 
 export default function Footer() {
-    return(
-        <Positioner>
-        <BlackBackground>
-            <FooterContents>
-                <Logo>Commento</Logo>
-            </FooterContents>
-        </BlackBackground>
-        </Positioner>
-    );
+  return (
+    <FooterContents>
+      <Logo>Commento</Logo>
+    </FooterContents>
+  );
 }
