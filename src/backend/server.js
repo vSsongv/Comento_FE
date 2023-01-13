feb344e9b3c58069b5053c1941efa205151fdb81
YuuuJeong <dbwjdgh03@ajou.ac.kr>
2023-01-13 07:33:00 +0000

    ec2

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,13 +4,14 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const db = require('./models');
-const webSocket = require('./socket');
+//const webSocket = require('./socket');
 
 // 라우터 부분
 
 const userRouter = require('./User/userRoute');
 const menteeRouter = require('./Mentee/menteeRoute');
 const mentoRouter = require('./Mento/mentoRoute');
+const chatRouter = require('./Chat/chatRoute');
 const errorhandler = require("./config/errorHandler");
 const errorResponse = require("./config/errorResponse");
 const { basicResponse } = require("./config/response");
@@ -48,13 +49,15 @@ const server = app.listen(app.get("port"), () => {
 });
 
 
-webSocket(server, app);
+//webSocket(server, app);
+
 
 //app.use('/', tempRouter);
 //라우터는 이사이에 표시
 app.use('/mento', mentoRouter);
 app.use('/user', userRouter);
 app.use('/mentee', menteeRouter);
+app.use('/', chatRouter);
 // app.use('/user/signup', checkRouter);
 // app.use('/user/upload', uploadRouter);
 // app.use('/find', findRouter);
@@ -71,4 +74,5 @@ app.use(errorhandler);
 app.use((req, res, next) => {
   const error = new errorResponse(basicResponse(responseDetail.NO_ROUTER), 404);
   next(error);
+
 });
