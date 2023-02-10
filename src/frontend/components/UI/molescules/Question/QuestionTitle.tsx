f79add8e39243bb557637c91d900c8b02d3f0066
김준하 <junhakjh@gmail.com>
2023-02-11 00:10:27 +0900

    feat: 질문 제목 작성, 제출버튼 구현

---
+++ src/frontend/components/UI/molescules/Question/QuestionTitle.tsx
@@ -0,0 +1,36 @@
+import React, { RefObject } from "react";
+import styled from "styled-components";
+import { border } from "../../../../styles/styleUtil";
+
+const Title = styled.p`
+  display: flex;
+  align-items: center;
+  margin: 0 3%;
+`;
+
+const TitleInput = styled.input`
+  height: 60%;
+  width: 55%;
+  font-size: 16px;
+  padding: 0.5rem;
+  border: none;
+  ${border(2)}
+  &:focus {
+    border-bottom: solid 1px;
+  }
+`;
+
+interface Props {
+  titleRef: RefObject<HTMLInputElement>;
+}
+
+function QuestionTitle(props: Props) {
+  return (
+    <>
+      <Title>제목</Title>
+      <TitleInput ref={props.titleRef} />
+    </>
+  );
+}
+
+export default QuestionTitle;
