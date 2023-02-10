735971bbf10c00eed30ad3a89230be9ee5618c0c
김준하 <junhakjh@gmail.com>
2023-02-10 21:05:50 +0900

    style: border top, right, bottom, left 선택할 수 있게 변경

--- src/frontend/styles/styleUtil.ts
+++ src/frontend/styles/styleUtil.ts
@@ -47,6 +47,21 @@ export const mainGradient = css`
   );
 `;
 
-export const bottomBorder = css`
-  border-bottom: solid 0.7px rgba(168, 168, 168, 1);
-`;
+export const border = (position: number) => {
+  const borders = [
+    css`
+      border-top: solid 0.7px rgba(168, 168, 168, 1);
+    `,
+    css`
+      border-right: solid 0.7px rgba(168, 168, 168, 1);
+    `,
+    css`
+      border-bottom: solid 0.7px rgba(168, 168, 168, 1);
+    `,
+    css`
+      border-left: solid 0.7px rgba(168, 168, 168, 1);
+    `,
+  ];
+
+  return borders[position];
+};
