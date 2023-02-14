57ce9d445d2f74b5a4b403da8fa208065a5dfa37
Dongyeon-Handsome <dongyeon4367@gmail.com>
2023-02-14 17:12:46 +0900

    feat: Add Scroll Button

--- src/frontend/components/pages/Home.tsx
+++ src/frontend/components/pages/Home.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import MainFirst from './Home/MainFirst';
-import FAQbox from './Home/FAQbox';
-import MainAdvantage from './Home/MainAdvantage';
-import MainBlack from './Home/MainBlack';
-import MainMentos from './Home/MainMentos';
-import MainPush from './Home/MainPush';
-import Qna from './Home/Qna';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import MainBtns from '../templates/Home/MainBtns';
+import MainFirst from '../templates/Home/MainFirst';
+import FAQbox from '../templates/Home/FAQbox';
+import MainAdvantage from '../templates/Home/MainAdvantage';
+import MainBlack from '../templates/Home/MainBlack';
+import MainMentos from '../templates/Home/MainMentos';
+import MainPush from '../templates/Home/MainPush';
+import Qna from '../templates/Home/Qna';
 
-function Home() {
+const ScrollBtn = styled.div`
+  position: fixed;
+  top: 80%;
+  left: 37%;
+  right: 37%;
+`;
+
+export default function Home() {
+  const [showBtn, setShowBtn] = useState(false);
+  const scrollBtn = () => {  
+    window.scroll({ behavior: 'smooth' })
+  }
+  useEffect(() => {
+    const ShowBtnClick = () => {
+      if(window.scrollY > 500){ setShowBtn(true) }
+      else{ setShowBtn(false) }
+    }
+    window.addEventListener("scroll", ShowBtnClick)
+    return () => {
+      window.removeEventListener("scroll", ShowBtnClick)
+    }
+  }, [])
+
   return (
     <div>
+      {showBtn && 
+        <div>
+          <ScrollBtn><MainBtns /></ScrollBtn>
+        </div>
+      }
       <MainFirst />
       <MainBlack />
       <Qna />
@@ -20,5 +49,3 @@ function Home() {
     </div>
   );
 }
-
-export default Home;
