a72d1bc45eeae434fa100895f32f329282847e2e
김준하 <junhakjh@gmail.com>
2023-02-16 16:26:09 +0900

    feat: 헤더 로그인 기본프로필 이미지 추가

--- src/frontend/components/molescules/HeaderLogin.tsx
+++ src/frontend/components/molescules/HeaderLogin.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import defaultProfile from '../../assets/images/defaultProfile.svg';
 
 const SignInLink = styled(Link)`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  width: 11rem;
   text-decoration: none;
   color: black;
 `;
 
+const SignInImage = styled.img`
+  width: 2.5rem;
+  height: 2.5rem;
+`;
+
 function HeaderLogin() {
-  return <SignInLink to='/signIn'>로그인이 필요합니다.</SignInLink>;
+  return (
+    <SignInLink to="/signIn">
+      로그인 해주세요.
+      <SignInImage src={defaultProfile} alt="프로필 사진" />
+    </SignInLink>
+  );
 }
 
 export default HeaderLogin;
