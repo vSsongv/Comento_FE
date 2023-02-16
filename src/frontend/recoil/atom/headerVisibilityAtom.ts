cd48f4ed32081b75217b0a0434e4e9459822f6c2
김준하 <junhakjh@gmail.com>
2023-02-16 17:01:14 +0900

    feat: 유저 정보 atom 추가

--- src/frontend/recoil/atom/headerVisibilityAtom.ts
+++ src/frontend/recoil/atom/headerVisibilityAtom.ts
@@ -1,6 +1,22 @@
-import { atom } from "recoil";
+import { atom } from 'recoil';
+import defaultProfile from '../../assets/images/defaultProfile.svg';
 
 export const headerVisibilityAtom = atom({
-  key: "headerVisibilityState",
+  key: 'headerVisibilityState',
   default: 1,
+});
+
+interface UserInfo {
+  name: string;
+  profileImage: string;
+  mentos: number;
+}
+
+export const userInfo = atom<UserInfo>({
+  key: 'UserInfo',
+  default: {
+    name: 'Comento',
+    profileImage: defaultProfile,
+    mentos: 0,
+  },
 });
