6544af54651f6ee95145db1c349e44a2229b217e
vSsongv <songdh21@naver.com>
2023-02-25 02:36:11 +0900

    ✨ feat: Api Sign module basic done

--- src/frontend/api/api.ts
+++ src/frontend/api/api.ts
@@ -1,5 +1,13 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: '//3.37.84.147:8080',
+  baseURL: '//3.37.84.147:8080', //TODO: process.env.REACT_APP_API_URL,
 });
+
+export const SignApi = {
+  signIn: () => api.post('user/signin'),
+  signUp: (userData: FormData) => api.post('user/signup', userData),
+  checkDupleEmail: () => api.get('user/check/email'),
+  checkDupleNick: () => api.get('user/check/nickname'),
+  checkDuplePhone: () => api.get('user/check/phone'),
+};
--- src/frontend/api/authService.ts
+++ src/frontend/api/authService.ts
@@ -1,1 +1,19 @@
-import axios from 'axios';
+import { SignApi } from './api';
+
+export type FormValue = {
+  email: string;
+  password: string;
+  password_signin: string;
+  password_confirm: string;
+  nickname: string;
+  phone: number;
+};
+
+export const SignUp = async (userData: FormData) => {
+  try {
+    const res = await SignApi.signUp(userData);
+    if (res.status === 200) return true;
+  } catch (error) {
+    console.log(error);
+  }
+};
