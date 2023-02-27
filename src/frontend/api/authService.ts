087718f1d67d4f3c26ec05d73d0b0242b87cc70f
vSsongv <songdh21@naver.com>
2023-02-28 04:32:02 +0900

    ✨ feat: signup api done

--- src/frontend/api/authService.ts
+++ src/frontend/api/authService.ts
@@ -1,15 +1,42 @@
 import { SignApi } from './api';
 
-export type FormValue = {
+export interface SignInProps {
   email: string;
-  password: string;
   password_signin: string;
+}
+
+export interface FormValue extends SignInProps {
+  password: string;
   password_confirm: string;
   nickname: string;
-  phone: number;
+  phone: string;
+}
+
+export const isDuple = async (purpose: string, target: FormValue) => {
+  try {
+    switch (purpose) {
+      case 'email': {
+        const res = await SignApi.checkDupleEmail(target.email);
+        alert(res.data.message);
+        return true;
+      }
+      case 'nickname': {
+        const res = await SignApi.checkDupleNick(target.nickname);
+        alert(res.data.message);
+        return true;
+      }
+      case 'phone': {
+        const res = await SignApi.checkDuplePhone(target.phone);
+        alert(res.data.message);
+        return true;
+      }
+    }
+  } catch (error: any) {
+    alert(error.response.data.message);
+  }
 };
 
-export const SignUp = async (userData: FormData) => {
+export const signUp = async (userData: FormData) => {
   try {
     const res = await SignApi.signUp(userData);
     if (res.status === 200) return true;
