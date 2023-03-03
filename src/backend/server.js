aef8ea7054ba92661d02b3cbcedb589164c5a47e
유정호 <64683286+YuuuJeong@users.noreply.github.com>
2023-03-04 01:27:51 +0900

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -20,7 +20,7 @@ const errorhandler = require("./config/errorHandler");
 const errorResponse = require("./config/errorResponse");
 const { basicResponse } = require("./config/response");
 const responseDetail = require("./config/responseDetail");
-//주석 추가
+//주석
 const app = express();
 
 const whiteDomain = [
