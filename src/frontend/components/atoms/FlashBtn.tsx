f5bd3b6a2df2d7b9d4012de714375c842095bf79
Dongyeon-Handsome <dongyeon4367@gmail.com>
2023-03-03 14:56:14 +0900

    fix: props 수정

--- src/frontend/components/atoms/FlashBtn.tsx
+++ src/frontend/components/atoms/FlashBtn.tsx
@@ -4,26 +4,24 @@ import { lighten, darken } from 'polished';
 
 type FlashBtnProps = {
   children: React.ReactNode;
-  color: string;
-  border: string;
-};
-
-FlashBtn.defaultProps = {
-  color: 'blue',
-  long: false,
-  border: true,
+  width: number;
+  borderRadius: number;
 };
 
 const StyledFlashBtn = styled.button<FlashBtnProps>`
   ${(props) => {
-    const COLOR = props.color;
-    const BORDER = props.border;
+    const WIDTH = props.width;
+    const BORDERRADIUS = props.borderRadius;
     return css`
       /* 공통 스타일 */
       display: inline-block;
       outline: none;
-      border: none;
-      color: white;
+      border: 3.5px solid transparent;
+      border-radius: ${BORDERRADIUS}px;
+      background-image: linear-gradient(#fff, #fff), linear-gradient(90deg, #033bff, #00e0ff);
+      background-origin: border-box;
+      background-clip: content-box, border-box;
+      color: #236AD6;
       font-weight: bold;
       font-family: 'NanumGothic';
       cursor: pointer;
@@ -32,23 +30,13 @@ const StyledFlashBtn = styled.button<FlashBtnProps>`
       margin-top: 20px;
       /* 크기 */
       padding: 1rem auto;
-      width: 6vw;
+      width: ${WIDTH}vw;
       height: 5vh;
       font-size: 1.4rem;
       /* 색상 */
       background-color: #FFFFFF;
-      ${BORDER &&
-      css`
-        color: ${COLOR};
-        background: none;
-        border: 5px solid transparent;
-        border-radius: 10px;
-        background-image: linear-gradient(#fff, #fff), linear-gradient(90deg, #033bff, #00e0ff);
-        background-origin: border-box;
-        background-clip: content-box, border-box;
-      `}
-      &:hover { color: ${lighten(0.2, COLOR)}; }
-      &:active { color: ${darken(0.2, COLOR)}; }
+      &:hover { color: ${lighten(0.1, 'blue')}; }
+      &:active { color: ${darken(0.1, 'blue')}; }
     `;
   }}
 `;
