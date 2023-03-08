9c045541708b03d46737dce9e516ea6e63cc8d53
김준하 <junhakjh@gmail.com>
2023-03-09 03:26:52 +0900

    update: refresh, signIn api의 인스턴스 분리

--- src/frontend/api/api.ts
+++ src/frontend/api/api.ts
@@ -4,24 +4,27 @@ import { SignInService } from './authService';
 export const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+export const auth = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
 
-export const Token = {
+export const Auth = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   refresh: (token: any) =>
-    api.get('user/refresh', {
+    auth.get('user/refresh', {
       headers: {
         'x-access-token': token,
       },
     }),
-};
-
-export const SignApi = {
   signIn: (userData: SignInService) =>
-    api.post('user/signin', {
+    auth.post('user/signin', {
       email: userData.email,
       password: userData.password_signin,
       isKeep: userData.isKeep,
     }),
+};
+
+export const SignApi = {
   signUp: (userData: FormData) => api.post('user/signup', userData),
   checkDupleEmail: (email: string) => api.get(`user/check/email?email=${email}`),
   checkDupleNick: (nickname: string) => api.get(`user/check/nickname?nickname=${nickname}`),
