9376e13c134c23180c77640eddf4172100c43568
vSsongv <songdh21@naver.com>
2023-03-12 03:12:30 +0900

    💄 style: del button padding

--- src/frontend/components/atoms/Button.tsx
+++ src/frontend/components/atoms/Button.tsx
@@ -34,7 +34,6 @@ const StyledButton = styled.button<ButtonProps>`
       margin-top: 20px;
       cursor: pointer;
       /* 크기 */
-      padding: 1rem 2rem;
       width: ${WIDTH}px;
       height: ${HEIGHT}px;
       font-size: ${FONT_SIZE}px;
