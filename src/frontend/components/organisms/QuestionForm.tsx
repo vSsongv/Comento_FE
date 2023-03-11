82e01f4239b2508ce5b46ab0e903ec7f23459a85
vSsongv <songdh21@naver.com>
2023-03-11 18:43:22 +0900

    💡 update: move folder

--- src/frontend/components/organisms/QuestionForm.tsx
+++ src/frontend/components/organisms/QuestionForm.tsx
@@ -7,7 +7,7 @@ import QuestionFile from '../molescules/Question/QeustionFile';
 import SubmitIcon from '../../assets/images/QuestionSubmit.svg';
 import DropDown from '../molescules/DropDown';
 import { Languages } from '../../utils/Languages';
-import { askQuestion } from '../../api/authService';
+import { askQuestion } from '../../api/menteeService';
 
 const QuestionBox = styled.div`
   display: flex;
