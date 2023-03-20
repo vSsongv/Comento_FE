17412c37813b6db6b1a5fab553b742d8ca3fcd7f
vSsongv <songdh21@naver.com>
2023-03-20 17:56:06 +0900

    ✨feat: list page question type btn view done

--- frontend/src/components/organisms/QuestionTypeNum.tsx
+++ frontend/src/components/organisms/QuestionTypeNum.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
+import colors from '../../styles/colors';
 import { questionType } from '../../recoil/atom';
 import { mainGradient } from '../../styles/styleUtil';
 
 const Container = styled.div`
   width: 850px;
-  height: 85px;
+  height: 100px;
   border: 1px solid rgb(168, 168, 168);
   border-radius: 10px;
   position: relative;
@@ -16,7 +17,7 @@ const Container = styled.div`
 const BottomGradient = styled.div`
   width: 100%;
   border-radius: 0px 0px 10px 10px;
-  height: 27px;
+  height: 30px;
   position: absolute;
   bottom: 0;
   display: flex;
@@ -34,19 +35,32 @@ const Type = styled.span`
 
 const Wrapper = styled.div`
   width: 848px;
-  height: 80px;
+  height: 70px;
   display: flex;
 `;
 
 const TypeButton = styled.button`
-  border-radius: 10px;
+  font-family: 'NanumGothic';
+  font-size: 14px;
+  font-weight: 400;
+  border-radius: 10px 10px 0px 0px;
   border: none;
   cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: white;
   flex: 1;
   :hover {
-    border: 1px solid #3c02bb;
+    border: 2px solid ${colors.firstColor};
   }
+`;
+
+const QuestionNum = styled.span`
+  font-family: 'NanumGothic';
+  font-size: 18px;
+  font-weight: 600;
+  margin-right: 5px;
 `;
 
 const QuestionTypeNum = () => {
@@ -55,9 +69,16 @@ const QuestionTypeNum = () => {
   return (
     <Container>
       <Wrapper>
-        <TypeButton onClick={() => setType(0)}>1</TypeButton>
-        <TypeButton onClick={() => setType(1)}>2</TypeButton>
-        <TypeButton onClick={() => setType(2)}>3</TypeButton>
+        {/* TODO: 질문 개수 */}
+        <TypeButton onClick={() => setType(0)}>
+          <QuestionNum>1</QuestionNum>건
+        </TypeButton>
+        <TypeButton onClick={() => setType(1)}>
+          <QuestionNum>1</QuestionNum>건
+        </TypeButton>
+        <TypeButton onClick={() => setType(2)}>
+          <QuestionNum>1</QuestionNum>건
+        </TypeButton>
       </Wrapper>
       <BottomGradient>
         <Type>매칭 전</Type>
